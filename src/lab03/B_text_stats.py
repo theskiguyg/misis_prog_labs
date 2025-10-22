d1bf99bf7f2d92aed3921259239599f41c33be9c
Zagrebin Egor <zagrebine@yandex.ru>
2025-10-22 11:13:20 +0300

    task B

--- src/lab03/B_text_stats.py
+++ src/lab03/B_text_stats.py
@@ -1,5 +1,16 @@
 import sys
-from src.lib.text import normalize, tokenize
-def main():
-    text = sys.stdin.read()
+from src.lib.text import count_freq, top_n, normalize, tokenize
+
+text = sys.stdin.read()
+
+tokens = tokenize(text=normalize(text=text))
+top = top_n(count_freq(tokens))
+
+print(f"Всего слов: {len(tokens)}")
+print(f"Уникальных слов: {len(set(tokens))}")
+print("Топ-5:")
+for w, c in top:
+    print(f"{w}:{c}")
+
+
     
